5bb695f3926f92ab4aaae4e3c27dbfcbcb70d87c
Kyle Grealis <kylegrealis@icloud.com>
2023-04-06 21:13:15 -0400

    removed or moved to other repo

--- git_pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# this script will iterate through the git repos and update them
-
-
-find ~/repos -maxdepth 1 -mindepth 1 -type d | while read dir; do
-  echo "$dir"
-  cd $dir && git pull
-done
